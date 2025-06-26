13885f423159d02990bcae23c8096e18e4f73b2b
katiay13 <myarkatia@gmail.com>
2025-06-25 18:27:32 -0700

    Created Client and Server Outlines

--- a2/mychatclient.py
+++ a2/mychatclient.py
@@ -1,1 +1,16 @@
 # Client program
+
+from socket import *
+import threading
+
+HOST = 'localhost'
+PORT = 2222
+BUFFER_SIZE = 1024
+
+# Function3: Receives messages from server and prints to user
+
+# Function2: Processes user input and sends to server
+
+# Function1: Create client socket and thread for receiving messages
+
+# Main function
--- a2/mychatserver.py
+++ a2/mychatserver.py
@@ -1,1 +1,19 @@
 # Server program
+
+from socket import *
+import threading
+
+PORT = 2222
+BUFFER_SIZE = 1024
+
+# Client = socket object, port number
+# Dictionary holds clients
+clients = {}
+
+# Function3: Send message to all chat users except sender
+
+# Function2: Receive message from a client and send to chat with funct3
+
+# Function1: Starts server and creates thread for each connected client
+
+# Main function
